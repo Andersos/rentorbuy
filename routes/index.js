919cf6b4ecaadde244cb75ac9dd878582d6058b1
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-02-17 21:57:15 +0100

    Now we have a game

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 module.exports = function (app) {
   var Router 		= require('koa-router');
   var router = new Router();
-    // var lifestyle = require('lifestyle');
-    // var client = new lifestyle.FinnClient("https://cache.api.finn.no/iad/", process.env.finn);
+  var lifestyle = require('lifestyle');
+  var client = new lifestyle.FinnClient("https://cache.api.finn.no/iad/", process.env.finn);
 
   router
   .get('/', function *() {
@@ -17,32 +17,38 @@ module.exports = function (app) {
   .get('/change', function *() {
     yield this.render('change');
   })
+  .get('/highscore', function *() {
+    yield this.render('highscore');
+  })
+  .get('/profile', function *() {
+    yield this.render('profile');
+  })
   .get('/house', function *() {
-      // getHouses();
+    //getHouses();
     yield this.render('house');
   });
-  /*
+/*
   function getHouses() {
-  client
-  .search('realestate-homes', { location: '1.20061.20512', ownership_type: 3, sort: 3 })
-  .then(function(result) {
-  result.entries.forEach(function (ad) {
-  return client.getAd(ad.adId);
-})
-//return client.getAd(adId);
-})
-.then(function(ad) {
-console.log(ad);
-//console.log(ad.title, ad.links.alternate);
-//ad.aData.general_text.forEach(function(e) {
-//console.log(e.value.replace(/<br \/>/g, "\n"))
-//});
-})
-.catch(function(err) {
-console.log("Error fetching ad:");
-console.log(err.stack);
-});
-}
+    client
+    .search('realestate-homes', { location: '0.20061', ownership_type: 3, sort: 3 })
+    .then(function(result) {
+      result.entries.forEach(function (ad) {
+        return client.getAd(ad.adId);
+      })
+      //return client.getAd(adId);
+    })
+    .then(function(ad) {
+      console.log(ad);
+      //console.log(ad.title, ad.links.alternate);
+      //ad.aData.general_text.forEach(function(e) {
+      //console.log(e.value.replace(/<br \/>/g, "\n"))
+      //});
+    })
+    .catch(function(err) {
+      console.log("Error fetching ad:");
+      console.log(err.stack);
+    });
+  }
 */
 
   app.use(router.middleware());
